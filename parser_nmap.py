f1324836c2ae452ee7ec041e0aa5ddccd8cd8c6c
Khazov Anton <ulowpie@gmail.com>
2023-05-30 21:15:04 +0500

    add directory, range ports, format

--- parser_nmap.py
+++ parser_nmap.py
@@ -4,9 +4,10 @@ import os
 import struct
 from socket import inet_aton
 from enum import Enum
-os.system("")   # enable color
 
-version = "v1.3.0"
+os.system("")  # enable color
+
+version = "v1.4.1"
 
 json_regex_types = {r'Nmap scan report for .*$': "0", r'^[0-9]*/tcp.*open.*': "0",
                     r'Aggressive OS guesses:.*': "0", r'PORT.*': "0", r'Device type:.*$': "0",
@@ -46,8 +47,7 @@ class ConstRegex(Enum):
 
 
 def drop_operation(msg):
-    print(f'{Colors.FAIL}{Status.ERROR.value} Drop operation: {msg}{Colors.END}')
-    exit()
+    return f'{Colors.FAIL}{Status.ERROR.value} Drop operation: {msg}{Colors.END}'
 
 
 def info_operation(msg):
@@ -66,13 +66,13 @@ def sort_ip(report_nmap):
 
 def is_found_line(regex, ports, line):
     if regex and ports:
-        if re.findall(ConstRegex.REG_PORT_FROM_STRING.value, line)[-1:][0] in ports and re.search(regex, line):
+        if int(re.findall(ConstRegex.REG_PORT_FROM_STRING.value, line)[-1:][0]) in ports and re.search(regex, line):
             return True
     elif regex:
         if re.search(regex, line):
             return True
     elif ports:
-        if re.findall(ConstRegex.REG_PORT_FROM_STRING.value, line)[-1:][0] in ports:
+        if int(re.findall(ConstRegex.REG_PORT_FROM_STRING.value, line)[-1:][0]) in ports:
             return True
     return False
 
@@ -92,6 +92,7 @@ def parse_scan(report_nmap, ports, regex, operation):
         if count_append:
             body.append(line_report)
             count_append = count_append - 1
+            continue
 
         for re_type, count_type_str in json_regex_types.items():
             if re.search(re_type, line_report):
@@ -148,6 +149,7 @@ def parse_scan(report_nmap, ports, regex, operation):
                         remember_title = line_report
                 else:
                     drop_operation('Unsupported parse operation')
+                    exit()
     if operation == Operation.ALL_REPORT:
         if count_open_ports:
             body.insert(0, remember_title_port)
@@ -159,86 +161,119 @@ def parse_scan(report_nmap, ports, regex, operation):
         return body
 
 
-def write_to_file(filename, data):
+def write_to_file(filename, data, format_out):
     print(info_operation(f'Generate report to {filename} file...'))
     with open(filename, 'w') as writer:
         if type(data) is list:
             for line in data:
                 writer.write(f"{line}\n")
         elif type(data) is dict:
-            for ip_str in data:
-                for line in data[ip_str]:
-                    writer.write(f"{line}\n")
-                writer.write('\n')
+            if format_out == 'min':
+                for ip_str in data:
+                    writer.write(f"{ip_str}\n")
+            elif format_out == 'max':
+                for ip_str in data:
+                    for line in data[ip_str]:
+                        writer.write(f"{line}\n")
+                    writer.write('\n')
+
         writer.write(f"Total IP parsed: {len(data)}")
+        print(info_operation(f"Total IP parsed: {len(data)}"))
 
 
-def print_target(data):
-    data[0] = header_operation(data[0])
-    data[-1] = header_operation(data[-1])
-    for line in data:
-        print(line)
+def print_target(data, format_out):
+    if format_out == 'max':
+        data[0] = header_operation(data[0])
+        data[-1] = header_operation(data[-1])
+        for line in data:
+            print(line)
+        print('')
+    elif format_out == 'min':
+        print(''.join(re.findall(ConstRegex.REG_IP_FROM_TITLE.value, data[0])[-1:]))
+    else:
+        drop_operation('Unsupported format')
+        return
 
 
-def print_scan(data):
+def print_scan(data, format_out):
     if type(data) is list:
         for line in data:
             print(line)
     elif type(data) is dict:
         for ip_str in data:
-            print('\n')
-            print_target(data[ip_str])
-    print(info_operation(f"Total IP parsed: {len(data)}"))
+            print_target(data[ip_str], format_out)
+    print(info_operation(f"Total IP: {len(data)}\n"))
+
+
+def get_ports(ports):
+    if ports:
+        result = []
+        for part in ports.split(','):
+            if '-' in part:
+                a, b = part.split('-')
+                a, b = int(a), int(b)
+                result.extend(range(a, b + 1))
+            else:
+                a = int(part)
+                result.append(a)
+        return result
+    return None
+
+
+def preprocedure(file, args):
+    report_nmap = open(file, 'r')
+    parse_reg_scan = []
+    if args.ip:
+        if args.ip == Operation.IP_VULNERABLE.value:
+            print(info_operation(f'Parsing VULNERABLE ip for {file}...'))
+            parse_reg_scan = sort_ip(parse_scan(report_nmap, None, None, Operation.IP_VULNERABLE))
+        elif args.ip == Operation.IP_ALL.value:
+            print(info_operation(f'Parsing ALL ip for {file}...'))
+            parse_reg_scan = sort_ip(parse_scan(report_nmap, None, None, Operation.IP_ALL))
+        elif args.ip == Operation.IP_OPEN.value:
+            print(info_operation(f'Parsing OPEN TCP ip for {file}...'))
+            parse_reg_scan = sort_ip(parse_scan(report_nmap, None, None, Operation.IP_OPEN))
+    else:
+        print(info_operation(f'Parsing {file}...'))
+        parse_reg_scan = parse_scan(report_nmap, get_ports(args.ports), args.regex, Operation.ALL_REPORT)
+        if args.target:
+            if parse_reg_scan.get(args.target):
+                print_target(parse_reg_scan[args.target], args.format)
+                return
+            else:
+                print(drop_operation(f"IP address in {file} not found\n"))
+                return
+    if args.output:
+        write_to_file(args.output, parse_reg_scan, args.format)
+    else:
+        print_scan(parse_reg_scan, args.format)
+    return
 
 
 def main():
-    parser = argparse.ArgumentParser(description="TCP Parser NMAP " + version)
+    parser = argparse.ArgumentParser(description="TCP Parser NMAP Normal output (-oN) " + version)
     parser.add_argument('-v', '-V', '--version', action='version', version=version)
-    parser.add_argument('-s', '--scan', required=True, help='report scan NMAP')
+    parser.add_argument('-s', '--scan', required=True, help='directory or report scan NMAP')
     parser.add_argument('-o', '--output', help='generate file report')
     parser.add_argument('-t', '--target', help='print-parsing for one ip')
     parser.add_argument('-p', '--ports', help='parsing for required ports')
     parser.add_argument('-r', '--regex', help='regex search to string port')
-    parser.add_argument('-i', '--ip', required=False, type=int, choices=range(1, 4), help="output ip:\
+    parser.add_argument('-f', '--format', type=str, choices=['min', 'max'], default='max', help='format output')
+    parser.add_argument('-i', '--ip', type=int, choices=range(1, 4), help="output ip:\
                                                             1 - for VULNERABLE ip;\
                                                             2 - for ALL ip;\
                                                             3 - for ip with OPEN TCP ports")
 
     args = parser.parse_args()
-    if not os.path.isfile(args.scan):
-        drop_operation(f'File {args.scan} not found')
-
-    report_nmap = open(args.scan, 'r')
-    parse_reg_scan = []
-    ports = None
-    regex = None
-    if args.ip:
-        if args.ip == Operation.IP_VULNERABLE.value:
-            print(info_operation('Parsing VULNERABLE ip...'))
-            parse_reg_scan = sort_ip(parse_scan(report_nmap, ports, regex, Operation.IP_VULNERABLE))
-        elif args.ip == Operation.IP_ALL.value:
-            print(info_operation(f'Parsing ALL ip...'))
-            parse_reg_scan = sort_ip(parse_scan(report_nmap, ports, regex, Operation.IP_ALL))
-        elif args.ip == Operation.IP_OPEN.value:
-            print(info_operation('Parsing OPEN TCP ip...'))
-            parse_reg_scan = sort_ip(parse_scan(report_nmap, ports, regex, Operation.IP_OPEN))
+    if os.path.isdir(args.scan):
+        for root, _, files in os.walk(args.scan):
+            for file in files:
+                preprocedure(os.path.join(root, file), args)
+    elif os.path.isfile(args.scan):
+        preprocedure(args.scan, args)
     else:
-        print(info_operation('Parsing...'))
-        if args.ports:
-            ports = list(str(args.ports).split(','))
-        if args.regex:
-            regex = args.regex
-        parse_reg_scan = parse_scan(report_nmap, ports, regex, Operation.ALL_REPORT)
-        if args.target:
-            if parse_reg_scan.get(args.target):
-                print_target(parse_reg_scan[args.target])
-                return
-            else:
-                drop_operation("IP address not found")
-    if args.output:
-        write_to_file(args.output, parse_reg_scan)
-    else:
-        print_scan(parse_reg_scan)
+        print(drop_operation(f'File/Dir {args.scan} not found'))
+        return
     print(info_operation('Done'))
     return
 
