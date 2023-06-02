f64cc497b5b08b13467698b1188bcd6b77f031d8
Khazov Anton <ulowpie@gmail.com>
2023-06-02 15:35:57 +0500

    setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='parser_nmap',
+    version='1.5.2',
+    url="https://github.com/redrockstyle/parser_nmap",
+    author="RedRockStyle",
+    description="TCP Parser NMAP Normal output (-oN) v1.5.2",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    zip_safe=False,
+    entry_points={
+        'console_scripts':
+            ['parser_nmap = parser_nmap.parser_nmap:main']
+    }
+)
